59684a9edf197bfc570daeeca0ee971d8d780d96
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-26 14:52:10 -0600

    Add files via upload

---
+++ Salas_Cristian_1214_3-W/Salas_Cristian_1214_3-W.py
@@ -0,0 +1,14 @@
+Apellido_P = "Salas" #Esta linea se encarga de definir la variable de el apellido paterno 
+Apellido_M = "De La O" #Esta linea se encarga de definir la variable de el apellido materno
+Nombre_1 = "Cristian" #Esta linea se encarga de definfir el primer nombre
+Nombre_2 = "David" #Esta linea se encargar de definir el segundo nombre
+Num_Control = "1214" #Esta linea se encarga de definir el numero de matricula
+Grado_Grupo = "3-W" #Esta linea se encarga de definir el grado y grupo
+print (Apellido_P, Apellido_M, Nombre_1, Nombre_2, Num_Control, Grado_Grupo ) #Esta linea se encarga de mostrar el conjunto de todas las variables
+Apellido_Pa = "SALAS" #Esta linea se encarga de definir la variable de el apellido paterno pero en mayusculas
+Apellido_Ma = "DE LA O" #Esta linea se encarga de definir la variable de el apellido materno pero en mayusculas
+Nombre_uno = "CRISTIAN" #Esta linea se encarga de definfir el primer nombre pero en mayusculas
+Nombre_dos = "DAVID" #Esta linea se encargar de definir el segundo nombre pero en mayusculas
+Num_Cont = "1214" #Esta linea se encarga de definir el numero de matricula
+Grado_Grp = "3-W" #Esta linea se encarga de definir el grado y grupo
+print (Apellido_Pa, Apellido_Ma, Nombre_uno, Nombre_dos, Num_Cont, Grado_Grp) #Esta linea se encarga de mostrar el conjunto de todas las variables pero en mayusculas
